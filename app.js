1dd7843b66d35d103bddfcb46bc5b52e310e3dd3
Binayak3666 <binayakhotta3666@gmail.com>
2022-06-02 10:44:09 +0530

    creating .env file for prefix url ---13.reading env

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 
+require('dotenv/config');
+const api = process.env.API_URL;
+
+//http://localhost:3030/api/v1/products
 app.get('/', (req, res, next)=>{
     res.send('hello Api')
 })
 
 app.listen(3030,()=>{
+    console.log(api)
     console.log("server is starting now in http://localhost:3030");
 })

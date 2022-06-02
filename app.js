7d6d41817c9b0429846e5909d15ca3d62c46ba17
Binayak3666 <binayakhotta3666@gmail.com>
2022-06-02 10:26:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res, next)=>{
+    res.send('hello Api')
+})
 
 app.listen(3030,()=>{
     console.log("server is starting now in http://localhost:3030");

5dc9ce1258878088fc8e48083037dfadcc63779b
Binayak3666 <binayakhotta3666@gmail.com>
2022-06-03 13:30:43 +0530

    insert/Add product category into data base--- 28 Add/delete part1

--- routers/categories.js
+++ routers/categories.js
@@ -11,4 +11,29 @@ router.get(`/`, async (req, res) =>{
     res.send(categoryList);
 })
 
+router.post('/',async (req, res)=>{
+    let category = new Category({
+        name: req.body.name,
+        icon: req.body.icon,
+        color: req.body.color
+    })
+    //one way to create
+    category = await category.save()
+    !category?res.status(404).send('category did\'t created!'):res.status(200).json({message: 'data insert successfully', data:category})     
+    
+    //second way to create
+    // category.save().then((resData)=>{
+    //     res.status(200).json({
+    //         message: 'data insert successfully',
+    //         data:resData
+    //     })
+    // }).catch((err)=>{
+    //     res.status(500).json({
+    //         message:err,
+    //         success: 'failed'
+    //     })
+    // })
+    
+    
+})
 module.exports =router;

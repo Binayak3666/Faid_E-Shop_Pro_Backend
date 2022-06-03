cd5a38cc90ea37b30c933f4cfb5c002e6ffae190
Binayak3666 <binayakhotta3666@gmail.com>
2022-06-03 13:52:11 +0530

    delete category from categories collection --28 Add/ delete part2

--- routers/categories.js
+++ routers/categories.js
@@ -18,22 +18,33 @@ router.post('/',async (req, res)=>{
         color: req.body.color
     })
     //one way to create
-    category = await category.save()
-    !category?res.status(404).send('category did\'t created!'):res.status(200).json({message: 'data insert successfully', data:category})     
+    // category = await category.save()
+    // !category?res.status(404).send('category did\'t created!'):res.status(200).json({message: 'data insert successfully', data:category})     
     
     //second way to create
-    // category.save().then((resData)=>{
-    //     res.status(200).json({
-    //         message: 'data insert successfully',
-    //         data:resData
-    //     })
-    // }).catch((err)=>{
-    //     res.status(500).json({
-    //         message:err,
-    //         success: 'failed'
-    //     })
-    // })
-    
-    
+    category.save().then((resData)=>{
+        res.status(200).json({
+            message: 'data insert successfully',
+            data:resData
+        })
+    }).catch((err)=>{
+        res.status(500).json({
+            message:err,
+            success: 'failed'
+        })
+    })    
 })
+
+router.delete('/:id',(req,res)=>{
+    Category.findByIdAndDelete(req.params.id).then((category)=>{
+        if(category){
+            res.status(200).json({success: true ,message:'the category deleted'})
+        }else{
+            res.status(404).json({success:false, message:'category not found'})
+        }
+    }).catch(err =>{
+        res.status(400).json({success: false, message: 'category not not found',error:err})
+    })
+})
+
 module.exports =router;

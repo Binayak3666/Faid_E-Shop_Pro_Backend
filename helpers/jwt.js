bafcf62f88c1e6a983ff9f9e3fc5c75fb490065f
Binayak3666 <binayakhotta3666@gmail.com>
2022-06-07 13:01:13 +0530

    Excluding REST API Routes From Authentication ---50

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,11 +1,19 @@
-const { expressjwt } = require('express-jwt');
+const { expressjwt } = require("express-jwt");
 
 function authJwt() {
-    const secret = process.env.secret;
-    return expressjwt({
-        secret,
-        algorithms: ['HS256']
-    })
+  const secret = process.env.secret;
+  const api = process.env.API_URL
+  return expressjwt({
+    secret,
+    algorithms: ["HS256"],
+  }).unless({
+    path: [
+        {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
+        {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
+        `${api}/users/login`,
+        `${api}/users/register`,
+    ],
+  });
 }
 
-module.exports = authJwt
+module.exports = authJwt;

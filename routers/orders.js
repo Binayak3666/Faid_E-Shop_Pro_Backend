f94ec07fcffde92a139895b148e15e22a28c7335
Binayak3666 <binayakhotta3666@gmail.com>
2022-06-08 09:39:30 +0530

    creating order schema and linked with orderitem and product ----58 link two collection

--- routers/orders.js
+++ routers/orders.js
@@ -1,14 +1,43 @@
-const {Order} = require('../models/order');
-const express = require('express');
+const { Orders } = require("../models/order");
+const express = require("express");
 const router = express.Router();
+const { OrderItems } = require("../models/order-item");
 
-router.get(`/`, async (req, res) =>{
-    const orderList = await Order.find();
+router.get(`/`, async (req, res) => {
+  const orderList = await Orders.find();
+  if (!orderList) {
+    res.status(500).json({ success: false });
+  }
+  res.send(orderList);
+});
 
-    if(!orderList) {
-        res.status(500).json({success: false})
-    } 
-    res.send(orderList);
-})
+router.post("/", async (req, res) => {
+    const orderItemsIds = req.body.orderItems.map(async orderitems =>{
+        let newOrderItem =  new OrderItems({
+            quantity: orderitems.quantity,
+            product: orderitems.product
+        })
+        newOrderItem = await newOrderItem.save();
+        return newOrderItem._id;
+    })
+    console.log(orderItemsIds)
+  let order = new Orders({
+    orderItems: orderItemsIds,
+    shippingAddress1: req.body.shippingAddress1,
+    shippingAddress2: req.body.shippingAddress2,
+    city: req.body.city,
+    zip: req.body.zip,
+    country: req.body.country,
+    phone: req.body.phone,
+    status: req.body.status,
+    totalPrice: req.body.totalPrice,
+    user: req.body.user,
+  });
+//   order = await order.save();
 
-module.exports =router;
+  if (!order) return res.status(400).send("the order cannot be created!");
+
+  res.send(order);
+});
+
+module.exports = router;

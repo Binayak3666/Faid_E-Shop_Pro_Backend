6c6c733e31fe8c78723fc4d65834438600e48d6f
Binayak3666 <binayakhotta3666@gmail.com>
2022-06-09 00:00:26 +0530

    multer, mime validation, single file upload api creation and validation if image is not there----70

--- routers/products.js
+++ routers/products.js
@@ -3,6 +3,31 @@ const { Category } = require("../models/category");
 const router = express.Router();
 const { Product } = require("../models/product");
 const mongoose = require("mongoose");
+const multer = require('multer');
+
+const FILE_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpeg',
+  'image/jpg':'jpg'
+}
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const isValid = FILE_TYPE_MAP[file.mimetype]
+    let uploadError = new Error('invalid image type')
+    if(isValid){
+      uploadError = null
+    }
+    cb(uploadError, "public/uploads")
+  },
+  filename: function (req, file, cb) {
+    const fileName = file.originalname.split(' ').join('-')
+    const extension = FILE_TYPE_MAP[file.mimetype]
+    cb(null, `${fileName}-${Date.now()}.${extension}`)
+  }
+})
+
+const uploadOptions = multer({ storage: storage })
 
 router.get("/", async (req, res, next) => {
   let filter = {};
@@ -24,15 +49,21 @@ router.get("/:id", async (req, res, next) => {
   res.send(product);
 });
 
-router.post(`/`, async (req, res) => {
+router.post(`/`,uploadOptions.single('image'), async (req, res) => {
   const category = await Category.findById(req.body.category);
   if (!category) return res.status(400).send("Invalid Category");
+
+  const file = req.file;  
+    if (!file) return res.status(400).send("there is no file attached please attach");     
+
+  const fileName = req.file.filename;
+  const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
 
   let product = new Product({
     name: req.body.name,
     description: req.body.description,
     richDescription: req.body.richDescription,
-    image: req.body.image,
+    image: `${basePath}${fileName}`,  // "http://localhost:3000/public/upload/image-2323232"
     brand: req.body.brand,
     price: req.body.price,
     category: req.body.category,
@@ -40,8 +71,9 @@ router.post(`/`, async (req, res) => {
     rating: req.body.rating,
     numReviews: req.body.numReviews,
     isFeatured: req.body.isFeatured,
-  });
+  });  
   product = await product.save();
+  console.log(product)
   if (!product) return res.status(500).send("The product cannot be created");
 
   res.send(product);

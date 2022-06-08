15f83c6be3f5f6820ebef53835cb350da6d46692
Binayak3666 <binayakhotta3666@gmail.com>
2022-06-09 00:22:10 +0530

    updating multiple image into our product

--- routers/products.js
+++ routers/products.js
@@ -3,31 +3,31 @@ const { Category } = require("../models/category");
 const router = express.Router();
 const { Product } = require("../models/product");
 const mongoose = require("mongoose");
-const multer = require('multer');
+const multer = require("multer");
 
 const FILE_TYPE_MAP = {
-  'image/png': 'png',
-  'image/jpeg': 'jpeg',
-  'image/jpg':'jpg'
-}
+  "image/png": "png",
+  "image/jpeg": "jpeg",
+  "image/jpg": "jpg",
+};
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const isValid = FILE_TYPE_MAP[file.mimetype]
-    let uploadError = new Error('invalid image type')
-    if(isValid){
-      uploadError = null
+    const isValid = FILE_TYPE_MAP[file.mimetype];
+    let uploadError = new Error("invalid image type");
+    if (isValid) {
+      uploadError = null;
     }
-    cb(uploadError, "public/uploads")
+    cb(uploadError, "public/uploads");
   },
   filename: function (req, file, cb) {
-    const fileName = file.originalname.split(' ').join('-')
-    const extension = FILE_TYPE_MAP[file.mimetype]
-    cb(null, `${fileName}-${Date.now()}.${extension}`)
-  }
-})
+    const fileName = file.originalname.split(" ").join("-");
+    const extension = FILE_TYPE_MAP[file.mimetype];
+    cb(null, `${fileName}-${Date.now()}.${extension}`);
+  },
+});
 
-const uploadOptions = multer({ storage: storage })
+const uploadOptions = multer({ storage: storage });
 
 router.get("/", async (req, res, next) => {
   let filter = {};
@@ -49,21 +49,22 @@ router.get("/:id", async (req, res, next) => {
   res.send(product);
 });
 
-router.post(`/`,uploadOptions.single('image'), async (req, res) => {
+router.post(`/`, uploadOptions.single("image"), async (req, res) => {
   const category = await Category.findById(req.body.category);
   if (!category) return res.status(400).send("Invalid Category");
 
-  const file = req.file;  
-    if (!file) return res.status(400).send("there is no file attached please attach");     
+  const file = req.file;
+  if (!file)
+    return res.status(400).send("there is no file attached please attach");
 
   const fileName = req.file.filename;
-  const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
+  const basePath = `${req.protocol}://${req.get("host")}/public/uploads/`;
 
   let product = new Product({
     name: req.body.name,
     description: req.body.description,
     richDescription: req.body.richDescription,
-    image: `${basePath}${fileName}`,  // "http://localhost:3000/public/upload/image-2323232"
+    image: `${basePath}${fileName}`, // "http://localhost:3000/public/upload/image-2323232"
     brand: req.body.brand,
     price: req.body.price,
     category: req.body.category,
@@ -71,9 +72,8 @@ router.post(`/`,uploadOptions.single('image'), async (req, res) => {
     rating: req.body.rating,
     numReviews: req.body.numReviews,
     isFeatured: req.body.isFeatured,
-  });  
+  });
   product = await product.save();
-  console.log(product)
   if (!product) return res.status(500).send("The product cannot be created");
 
   res.send(product);
@@ -141,5 +141,36 @@ router.get("/get/featured/:count", async (req, res, next) => {
   }
   res.send({ product: product });
 });
+
+router.put(
+  "/gallery-images/:id",
+  uploadOptions.array("images", 10),
+  async (req, res) => {
+    if (!mongoose.isValidObjectId(req.params.id)) {
+      return res.status(400).send("Invalid Product Id");
+    }
+    const files = req.files;
+    let imagesPaths = [];
+    const basePath = `${req.protocol}://${req.get("host")}/public/uploads/`;
+
+    if (files) {
+      files.map((file) => {
+        imagesPaths.push(`${basePath}${file.filename}`);
+      });
+    }
+
+    const product = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        images: imagesPaths,
+      },
+      { new: true }
+    );
+
+    if (!product) return res.status(500).send("the gallery cannot be updated!");
+
+    res.send(product);
+  }
+);
 
 module.exports = router;
